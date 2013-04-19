8424f29b0b1b6fff4fdf6e09880b179edea9d211
PaNgia Vang <pangia.v@gmail.com>
2013-04-18 19:41:09 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Booksmark::Application.config.secret_token = '0d203b8adfb883dec29249efdbaaacc95a1e740ad41af105bce35a98abd14b726eea88819a344b557778f771d9073f67e7261d6aafbe89b3a00accf5f16ec7a3'
